e3a2da4cf8056ea88bc876068d938a82656deb63
Matteo Di Gallo <m.digallo@campus.unimib.it>
2022-06-16 17:08:40 +0200

    branch destro

--- app/src/main/java/it/sal/disco/unimib/git_test/MainActivity.java
+++ app/src/main/java/it/sal/disco/unimib/git_test/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         // commento per il secondo branch di GIT
+        // commento per il branch destro
     }
 }
