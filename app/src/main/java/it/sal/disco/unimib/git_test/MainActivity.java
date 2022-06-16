6823f25de07d03596efbab7486e09895c152c78b
Matteo Di Gallo <m.digallo@campus.unimib.it>
2022-06-16 10:50:58 +0200

    Nuovo commento nel programma

--- app/src/main/java/it/sal/disco/unimib/git_test/MainActivity.java
+++ app/src/main/java/it/sal/disco/unimib/git_test/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // commento per il secondo branch di GIT
     }
 }
